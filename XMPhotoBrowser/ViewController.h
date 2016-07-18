be34d27cdca2b5f57d5aa123b657e171309e717c
unknownDeep <x0000_00_00@163.com>
2016-07-18 10:48:27 +0800

    Initial Commit

---
+++ XMPhotoBrowser/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  XMPhotoBrowser
+//
+//  Created by zhanghao on 16/7/18.
+//  Copyright © 2016年 zhanghao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
